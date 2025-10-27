76bf3dd141ec2c1c320892b24a4fd79fb343bd60
Milenah <mraharimalala03@gmail.com>
2025-10-27 23:39:09 +0300

    ajout de vocaux

--- data/lessons/lesson1.js
+++ data/lessons/lesson1.js
@@ -1,63 +1,75 @@
 export default {
-  id: 1,
-  title: "Salutations de base",
-  subtitle: "Apprenez à dire bonjour et au revoir en malgache",
-  steps: [
-    {
-      id: 1,
-      type: "introduction",
-      title: "Bienvenue dans cette leçon !",
-      content:
-        "Dans cette leçon, vous allez apprendre les salutations de base en malgache. Prêt à commencer ?",
-      image: require("../../assets/auth-vector.png"),
-    },
-    {
-      id: 2,
-      type: "vocabulary",
-      title: "Salutations de base",
-      content: [
-        { 
-          malagasy: "Tongasoa", 
-          french: "Bonjour / Bienvenue", 
-          pronunciation: "Tong-a-so-a",
-          // audioFile: require("../../assets/vocaux/tongasoa.mp3") // Décommenter quand le fichier audio sera ajouté
+    id: 1,
+    title: "Salutations de base",
+    subtitle: "Apprenez à dire bonjour et au revoir en malgache",
+    steps: [
+        {
+            id: 1,
+            type: "introduction",
+            title: "Bienvenue dans cette leçon !",
+            content:
+                "Dans cette leçon, vous allez apprendre les salutations de base en malgache. Prêt à commencer ?",
+            image: require("../../assets/auth-vector.png"),
         },
-        { 
-          malagasy: "Salama", 
-          french: "Salut", 
-          pronunciation: "Sa-la-ma",
-          // audioFile: require("../../assets/vocaux/salama.mp3") // Décommenter quand le fichier audio sera ajouté
+        {
+            id: 2,
+            type: "vocabulary",
+            title: "Salutations de base",
+            content: [
+                {
+                    malagasy: "Tongasoa",
+                    french: "Bonjour / Bienvenue",
+                    pronunciation: "Tong-a-so-a",
+                    audioFile: require("../../assets/vocaux/Tongasoa.m4a"), // Décommenter quand le fichier audio sera ajouté
+                },
+                {
+                    malagasy: "Salama",
+                    french: "Salut",
+                    pronunciation: "Sa-la-ma",
+                    audioFile: require("../../assets/vocaux/Salama.m4a") // Décommenter quand le fichier audio sera ajouté
+                },
+                {
+                    malagasy: "Veloma",
+                    french: "Au revoir",
+                    pronunciation: "Ve-lo-ma",
+                    audioFile: require("../../assets/vocaux/Veloma.m4a") // Décommenter quand le fichier audio sera ajouté
+                },
+                {
+                    malagasy: "Misaotra",
+                    french: "Merci",
+                    pronunciation: "Mi-sao-tra",
+                    audioFile: require("../../assets/vocaux/Misaotra.m4a") // Décommenter quand le fichier audio sera ajouté
+                },
+            ],
         },
-        { 
-          malagasy: "Veloma", 
-          french: "Au revoir", 
-          pronunciation: "Ve-lo-ma",
-          // audioFile: require("../../assets/vocaux/veloma.mp3") // Décommenter quand le fichier audio sera ajouté
+        {
+            id: 3,
+            type: "quiz",
+            title: "Testez vos connaissances",
+            questions: [
+                {
+                    question: "Comment dit-on 'Bonjour' en malgache ?",
+                    options: ["Tongasoa", "Salama", "Veloma", "Misaotra"],
+                    correct: 1,
+                },
+                {
+                    question: "Que signifie 'Veloma' ?",
+                    options: ["Bonjour", "Merci", "Au revoir", "Salut"],
+                    correct: 2,
+                },
+                {
+                    question: "Comment dit-on 'Merci' en malgache ?",
+                    options: ["Tongasoa", "Salama", "Misaotra", "Veloma"],
+                    correct: 2,
+                },
+            ],
         },
-        { 
-          malagasy: "Misaotra", 
-          french: "Merci", 
-          pronunciation: "Mi-sao-tra",
-          // audioFile: require("../../assets/vocaux/misaotra.mp3") // Décommenter quand le fichier audio sera ajouté
+        {
+            id: 4,
+            type: "practice",
+            title: "Pratiquez les salutations",
+            content: "Répétez après nous : Tongasoa, Salama, Veloma, Misaotra",
+            audioHint: "Cliquez pour écouter la prononciation",
         },
-      ],
-    },
-    {
-      id: 3,
-      type: "quiz",
-      title: "Testez vos connaissances",
-      questions: [
-        { question: "Comment dit-on 'Bonjour' en malgache ?", options: ["Tongasoa", "Salama", "Veloma", "Misaotra"], correct: 1 },
-        { question: "Que signifie 'Veloma' ?", options: ["Bonjour", "Merci", "Au revoir", "Salut"], correct: 2 },
-        { question: "Comment dit-on 'Merci' en malgache ?", options: ["Tongasoa", "Salama", "Misaotra", "Veloma"], correct: 2 },
-      ],
-    },
-    {
-      id: 4,
-      type: "practice",
-      title: "Pratiquez les salutations",
-      content: "Répétez après nous : Tongasoa, Salama, Veloma, Misaotra",
-      audioHint: "Cliquez pour écouter la prononciation",
-    },
-  ],
+    ],
 };
--- data/lessons/lesson2.js
+++ data/lessons/lesson2.js
@@ -1,40 +1,59 @@
 export default {
-  id: 2,
-  title: "Les nombres",
-  subtitle: "Apprenez à compter en malgache jusqu’à 10",
-  steps: [
-    {
-      id: 1,
-      type: "introduction",
-      title: "Compter en malgache",
-      content: "Dans cette leçon, vous apprendrez à dire les chiffres de 1 à 10.",
-      image: require("../../assets/auth-vector.png"),
-    },
-    {
-      id: 2,
-      type: "vocabulary",
-      title: "Les nombres de 1 à 10",
-      content: [
-        { malagasy: "Isa iray", french: "Un", pronunciation: "Isa i-ray" },
-        { malagasy: "Roa", french: "Deux", pronunciation: "Ro-a" },
-        { malagasy: "Telo", french: "Trois", pronunciation: "Te-lo" },
-        { malagasy: "Efatra", french: "Quatre", pronunciation: "E-fa-tra" },
-        { malagasy: "Dimy", french: "Cinq", pronunciation: "Di-my" },
-        { malagasy: "Enina", french: "Six", pronunciation: "E-ni-na" },
-        { malagasy: "Fito", french: "Sept", pronunciation: "Fi-to" },
-        { malagasy: "Valo", french: "Huit", pronunciation: "Va-lo" },
-        { malagasy: "Sivy", french: "Neuf", pronunciation: "Si-vy" },
-        { malagasy: "Folo", french: "Dix", pronunciation: "Fo-lo" },
-      ],
-    },
-    {
-      id: 3,
-      type: "quiz",
-      title: "Petit quiz sur les nombres",
-      questions: [
-        { question: "Comment dit-on '3' en malgache ?", options: ["Roa", "Telo", "Efatra", "Dimy"], correct: 1 },
-        { question: "Que signifie 'Sivy' ?", options: ["Sept", "Neuf", "Huit", "Dix"], correct: 1 },
-      ],
-    },
-  ],
+    id: 2,
+    title: "Les nombres",
+    subtitle: "Apprenez à compter en malgache jusqu’à 10",
+    steps: [
+        {
+            id: 1,
+            type: "introduction",
+            title: "Compter en malgache",
+            content:
+                "Dans cette leçon, vous apprendrez à dire les chiffres de 1 à 10.",
+            image: require("../../assets/auth-vector.png"),
+        },
+        {
+            id: 2,
+            type: "vocabulary",
+            title: "Les nombres de 1 à 10",
+            content: [
+                {
+                    malagasy: "Isa/iray",
+                    french: "Un",
+                    pronunciation: "Isa i-ray",
+                    audioFile: require("../../assets/vocaux/Isa.m4a"),
+                },
+                { malagasy: "Roa", french: "Deux", pronunciation: "Ro-a", audioFile: require("../../assets/vocaux/Roa.m4a") },
+                { malagasy: "Telo", french: "Trois", pronunciation: "Te-lo", audioFile: require("../../assets/vocaux/Telo.m4a") },
+                {
+                    malagasy: "Efatra",
+                    french: "Quatre",
+                    pronunciation: "E-fa-tra",
+                    audioFile: require("../../assets/vocaux/Efatra.m4a"),
+                },
+                { malagasy: "Dimy", french: "Cinq", pronunciation: "Di-my", audioFile: require("../../assets/vocaux/Dimy.m4a") },
+                { malagasy: "Enina", french: "Six", pronunciation: "E-ni-na", audioFile: require("../../assets/vocaux/Enina.m4a") },
+                { malagasy: "Fito", french: "Sept", pronunciation: "Fi-to", audioFile: require("../../assets/vocaux/Fito.m4a") },
+                { malagasy: "Valo", french: "Huit", pronunciation: "Va-lo", audioFile: require("../../assets/vocaux/Valo.m4a") },
+                { malagasy: "Sivy", french: "Neuf", pronunciation: "Si-vy", audioFile: require("../../assets/vocaux/Sivy.m4a") },
+                { malagasy: "Folo", french: "Dix", pronunciation: "Fo-lo",  audioFile: require("../../assets/vocaux/Folo.m4a") },
+            ],
+        },
+        {
+            id: 3,
+            type: "quiz",
+            title: "Petit quiz sur les nombres",
+            questions: [
+                {
+                    question: "Comment dit-on '3' en malgache ?",
+                    options: ["Roa", "Telo", "Efatra", "Dimy"],
+                    correct: 1,
+                },
+                {
+                    question: "Que signifie 'Sivy' ?",
+                    options: ["Sept", "Neuf", "Huit", "Dix"],
+                    correct: 1,
+                },
+            ],
+        },
+    ],
 };
